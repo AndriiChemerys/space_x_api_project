9409447d2158e118f1f3b003e03b3fe44593d06d
AndriiChemerys <andriichemerys@gmail.com>
2020-09-29 21:35:44 +0200

    Creating Launch Timeline component

---
+++ src/components/LaunchTimelineItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-timeline
+      :reverse="reverse"
+      :dense="$vuetify.breakpoint.smAndDown"
+    >
+      <v-timeline-item
+        v-for="n in 2"
+        :key="n"
+      >
+        <span slot="opposite">Tus eu perfecto</span>
+        <v-card class="elevation-2">
+          <v-card-title class="headline">
+            Lorem ipsum
+          </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
