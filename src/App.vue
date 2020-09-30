6dd52866829fd5b5fac1595a0d1ed564fe8b7b0e
AndriiChemerys <andriichemerys@gmail.com>
2020-09-30 20:59:15 +0200

    Creaitng timeline with SpaceX API

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <h2>SpaceX Timeline</h2>
+        <h2>SpaceX Launches</h2>
       </div>
 
       <v-spacer></v-spacer>
-
     </v-app-bar>
 
-    <v-main>
-
-    </v-main>
+    <v-content>
+      <v-container>
+        <v-timeline v-if="launches.length > 0">
+          <LaunchTimeLineItem
+            v-for="launch in launches"
+            :key="launch.flight_number"
+            :launch="launch"
+          />
+        </v-timeline>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 import axios from "axios";
+import LaunchTimeLineItem from "./components/LaunchTimeLineItem";
 export default {
   name: "App",
-
-  components: {},
-
+  components: {
+    LaunchTimeLineItem,
+  },
   data: () => ({
     launches: [],
   }),
   async created() {
-    const { data } = axios.get("https://api.spacexdata.com/v4/launches");
+    const { data } = await axios.get(
+      "https://api.spacexdata.com/v3/launches/past"
+    );
 
     data.forEach(launch => {
       this.launches.push(launch);
--- src/components/LaunchTimelineItem.vue
+++ src/components/LaunchTimelineItem.vue
@@ -1,27 +1,42 @@
 <template>
-    <v-timeline
-      :reverse="reverse"
-      :dense="$vuetify.breakpoint.smAndDown"
-    >
-      <v-timeline-item
-        v-for="n in 2"
-        :key="n"
-      >
-        <span slot="opposite">Tus eu perfecto</span>
-        <v-card class="elevation-2">
-          <v-card-title class="headline">
-            Lorem ipsum
-          </v-card-title>
-          <v-card-text>
-            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-          </v-card-text>
-        </v-card>
-      </v-timeline-item>
-    </v-timeline>
+  <v-timeline class="timeline" :reverse="reverse" :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item large class="timeline__item" v-for="n in 2" :key="n">
+      <template v-slot:icon>
+        <v-avatar>
+          <img class="timeline__img" :src="launch.links.mission_patch_small" alt="">
+        </v-avatar>
+      </template>
+      <template>
+        <span slot="opposite">
+          {{launchDate}}
+        </span>
+      </template>
+      <v-card class="elevation-2">
+        <v-card-title class="headline">
+          MISSION: {{launch.mission_name}}
+        </v-card-title>
+        <v-card-subtitle>
+          ROCKET: {{launch.rocket.rocket_name}}
+        </v-card-subtitle>
+        <v-card-text>
+          {{launch.details}}
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
 <script>
-export default {};
+  import moment from "moment";
+
+  export default {
+    props: ["launch"],
+    computed: {
+      launchDate() {
+        return moment(this.launch.launch_date_local).format("MM/DD/YYYY");
+      },
+    },
+  };
 </script>
 
 <style>
